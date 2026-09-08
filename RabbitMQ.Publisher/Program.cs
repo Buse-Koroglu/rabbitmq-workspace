129384559f033cb12e934c8e1a9fa1e86274e915
Buse Koroglu <buse.krgl20@gmail.com>
2026-09-08 10:38:11 +0300

    implemented basic rabbitmq publisher and subscriber and default exchange for now

---
+++ RabbitMQ.Publisher/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+// rabbitmq bağlantı bilgileri
+var factory = new ConnectionFactory{ HostName = "localhost", Port = 5672 , UserName = "guest", Password = "guest" };
+
+// rabbitmq sunucusuna bağlanmak ve kanal açmak için
+await using var connection = await factory.CreateConnectionAsync();
+await using var channel = await connection.CreateChannelAsync();
+
+// queue olması gerektiği için bir queue tanımlıyorum
+await channel.QueueDeclareAsync(queue:"Hello-Queue", durable: true, /* RabbitMQ yeniden başlarsa kuyruk silinmesin */ exclusive: false, autoDelete: false,arguments:null);
+// autoDelete true olursa eğer o queueyu dinleyen bir tane bile consumer olmazsa queue'yu otomatik siler. exclusive true olursa queue'ya farklı servisler tarafından erişilemez.
+
+var properties = new BasicProperties();
+properties.Persistent = true;
+
+foreach (var x in Enumerable.Range(1, 50))
+{
+    string message = "Message" + x;
+    var body = Encoding.UTF8.GetBytes(message);
+
+    await channel.BasicPublishAsync(
+        exchange: string.Empty, 
+        routingKey: "Hello-Queue", 
+        mandatory: true, 
+        basicProperties: properties, 
+        body: body
+    );
+
+    Console.WriteLine($"Mesaj gönderildi: {message}");
+}
+
+Console.ReadLine();
---
+++ RabbitMQ.Subscriber/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory{ HostName = "localhost", Port = 5672 , UserName = "guest", Password = "guest" };
+
+// rabbitmq sunucusuna bağlanmak ve kanal açmak için
+await using var connection = await factory.CreateConnectionAsync();
+await using var channel = await connection.CreateChannelAsync();
+
+// queue'yu zaten publisher tarafından tanımlamıştık 
+// await channel.QueueDeclareAsync(queue:"Hello-Queue", durable: true, /* RabbitMQ yeniden başlarsa kuyruk silinmesin */ exclusive: false, autoDelete: false,arguments:null);
+
+await channel.BasicQosAsync(0, 1, false); // BasicQosAsync ifadesi ile bir consumer'ın işlenmesi için alabileceği maksimum mesaj sayısını belirtmeye yarayan bir akış kontrolü sağlar. 
+/* prefetchSize: bana herhangi boyutta bir mesaj gönderebilrsin. */
+/* prefetchCount: her bir subscriber'a kaçar kaçar mesaj gelsin onun bilgisini verir */
+/* global: true olursa prefetchCount'un tamamını subscriber sayısına göre dağıtır ancak false verirsek tüm subscriber'lara prefetchCount kadar gönderir. */
+
+var consumer = new AsyncEventingBasicConsumer(channel);
+
+consumer.ReceivedAsync += async (sender, ea) =>
+{
+    var message = Encoding.UTF8.GetString(ea.Body.Span);
+    Thread.Sleep(1500);
+    Console.WriteLine("GELEN MESAJ: " + message);
+    await channel.BasicAckAsync(ea.DeliveryTag /* mesajın unique sıra id'si*/, multiple: false /* sadece teslim edilen mesajın queue'dan çıkarılacağını diğer mesajlara dokunulmayacağını garantiler.*/ );
+};
+
+await channel.BasicConsumeAsync("Hello-Queue", false, consumer);  // auto-ack ben sana mesajın sileneceğini haber edeceğim demektir.
+
+Console.ReadLine();
+
