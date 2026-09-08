be4a2f383df44e47d68a4300d860adf030344e51
Buse Koroglu <buse.krgl20@gmail.com>
2026-09-08 13:38:00 +0300

    implemented fanout exchange type with producer withı-out a queue and consumer with a not-persistent queue for more than 1 subscriber

---
+++ RabbitMQ.FanoutExchangePublisher/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory
+{
+    HostName = "localhost",
+    Port = 5672,
+    UserName = "guest",
+    Password = "guest"
+};
+
+await using var connection = await factory.CreateConnectionAsync();
+await using var channel = await connection.CreateChannelAsync();
+
+await channel.ExchangeDeclareAsync(
+    "logs-fanout",
+    durable: true,
+    type: ExchangeType.Fanout
+);
+
+var properties = new BasicProperties
+{
+    Persistent = true
+};
+
+foreach (var x in Enumerable.Range(1, 50))
+{
+    string message = "Log" + x;
+    var body = Encoding.UTF8.GetBytes(message);
+
+    await channel.BasicPublishAsync(
+        exchange: "logs-fanout",
+        routingKey: "",
+        mandatory: true,
+        basicProperties: properties,
+        body: body
+    );
+
+    Console.WriteLine($"Log gönderildi: {message}");
+}
+
+Console.ReadLine();
---
+++ RabbitMQ.FanoutExchangeSubscriber/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory
+{
+    HostName = "localhost",
+    Port = 5672,
+    UserName = "guest",
+    Password = "guest"
+};
+
+await using var connection = await factory.CreateConnectionAsync();
+await using var channel = await connection.CreateChannelAsync();
+
+// EĞER KALICI BİR QUEUE OLUŞTURMAK İSTERSEK RANDOM QUEUE İSMİ VERMEYİ TERCİH ETMEYİZ.
+ var queueDeclareResult = await channel.QueueDeclareAsync();
+ var randomQueueName = queueDeclareResult.QueueName;
+
+// Eğer kalıcı queue oluşturmak istersek 
+// var queueName = "database-logs-queue";
+// await channel.QueueDeclareAsync(queue: queueName, durable: true, exclusive: false, false);
+
+await channel.QueueBindAsync(
+    queue: randomQueueName,
+    exchange: "logs-fanout",
+    routingKey: "",
+    arguments: null
+);
+
+await channel.BasicQosAsync( prefetchSize: 0, prefetchCount: 1, global: false );
+
+var consumer = new AsyncEventingBasicConsumer(channel);
+
+
+consumer.ReceivedAsync += async (sender, ea) =>
+{
+    var message = Encoding.UTF8.GetString(ea.Body.Span);
+
+    await Task.Delay(1500);
+
+    Console.WriteLine("GELEN LOG: " + message);
+
+    await channel.BasicAckAsync( deliveryTag: ea.DeliveryTag, multiple: false);
+};
+
+await channel.BasicConsumeAsync(queue: randomQueueName, autoAck: false, consumer: consumer);
+
+Console.ReadLine();
